5b2748e77d625a2d5cfd9f6103d97545b59a102a
buladou <lalasahara@outlook.com>
2022-04-18 16:20:15 +0900

    third time python file

---
+++ ifelse.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+__author__='tang'
+
+'''age=17
+if age>18:
+    print('adout')
+else:
+    print('kids')
+#print(111) '''
+
+'''score = 65
+if score >= 70:
+    print('excellt')
+elif score >=60:
+    print('safe')
+else:
+    print('try again')  '''
+
+'''
+if 'abc':
+    print('write for short') '''
+
+
+#print 1+2+...+100
+
+''' 
+i=1
+sum=0
+while i<=100:
+    sum += i 
+    i += 1
+print(sum) #5050
+'''
+
+'''names = ['tom', 'jack', 'alice', 'mike']
+for name in names:
+    print(name,end=',') '''
+'''for i in range(1,100):
+    print(i,end=',')  
+print()'''
+
+'''for i in range(100):
+    print(i,end=',') '''
+'''for i in range(1,100,2):
+    print(i,end=',')  # 1,3,5,7,...,99  
+print() '''
+
+'''sum=0
+for i in range(1,101):
+    sum += i
+print(sum) #5050 '''
+
+'''
+nums=[11,4,3,23,64,2,236,8]
+for num in nums:
+    print(num,end=',')
+print()
+for i in range(len(nums)):
+    print(i,nums[i])
+
+for n in nums[1:]:
+    print(n,end=',')
+'''
+for i in range(1,101):
+    if i%2 == 1 :
+        continue
+    print(i,end=',')
+print()
+
+
+
+
