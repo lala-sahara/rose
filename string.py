afd4e2934dd865ea91361c9e4ad3ea80889dfa22
buladou <lalasahara@outlook.com>
2022-04-18 09:29:35 +0900

    first python file

---
+++ string.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__="tang"
+#step 1.add the line on first line
+#step 2.chmod +x hello.py
+#print ('hello world')
+#name='alice'
+#print(name)
+#use IDE tool
+# use pyCharm
+#print(2, 5, 8,sep="-", end=' ')
+#print (18)
+'''age='18'
+name=input ('please enter your name:')
+print ('\n your name is '+name + '; your age is ' + age)'''
+#a=5
+#b=3
+#b='3'
+#print (a+b)
+#print (a,b)
+#print (b,type(b))
+#print ('she says "i like you." and then left.'
+#talk=print (''' she says:
+#I hate you!
+#     ......for ever!
+#          and then , gone;''')
+#print(talk)
+#f=True
+#print (f,type(f))
+''''g=5<3
+print (g,type(g))
+m=None
+print (m,type(m))'''
+'''a='25'
+b=int(a)
+#print(a,b,type(a),type(b))
+print(str(b)+a)'''
+
+#string='  hello world   '
+#print(string.islower())
+#print(string.isupper())
+#print(string.capitalize())
+#print(string.index('llo'))
+#print(string)
+#print(string.strip())
+#print(len(string))
+#name='tom cruise'
+#print(name[0]
+#print(name[4],name[len(name) - 1] ,name[-1])
+#print(name[1:5])
+#print(name[:5])
+#print(name[2:])
+#print(name[1:8:2])
+#print(name[::2])
+
+'''name='tomcruise'
+age=18
+height=180.526'''
+#print('hello,my name is ' + name +',age is '+ str(age) +',height is ' + str(height))
+#print('hello,my name is %s ,my age is %s ,my height is %s ,nice to meet you!' % (name,age,height))
+#print('hello,my name is %2.4s ,my age is %d ,my height is %.2f ' % (name,age,height))
+''' hello,my name is tomcruise,age is 18,height is 180.526
+hello,my name is tomcruise ,my age is 18 ,my height is 180.526 ,nice to meet you!
+hello,my name is tomc ,my age is 18 ,my height is 180.53 
+
+ '''
+#print('now is : %dyear-%02dmonth-%dday' % (2018,5,9))
+# now is : 2018year-05month-9day
+
+#print('hello,there~ my name is {0}, age is {1} ,height is {2} , nice meet you guys.' .format(name,age,height))
+## hello,there~ my name is tomcruise, age is 18 ,height is 180.526 , nice meet you guys.
+#print('hello,there~ my name is {0}, age is {1} ,height is {2:.2f} , nice meet you guys.' .format(name,age,height))
+# hello,there~ my name is tomcruise, age is 18 ,height is 180.53 , nice meet you guys.
+
+#print('hello,there~ my name is {name}, age is {age} ,height is {height} , have fun.' .format(name=name,age=age,height=height))
+# hello,there~ my name is tomcruise, age is 18 ,height is 180.526 , have fun.
+
+#print(f'hi,my name is {name},age is {age}, height is {height}.seeya~')
+#hi,my name is tomcruise,age is 18, height is 180.526.seeya~
