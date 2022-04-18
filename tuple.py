b0e913a9465956c058609ac7c84a4c74a3d5e550
buladou <lalasahara@outlook.com>
2022-04-18 14:59:08 +0900

    second python file

---
+++ tuple.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#print(3+5)
+#print(3*5)
+#print(30*'-')
+# ------------------------------
+
+#print(5/3)
+#print(5%3)
+#print(10//3)
+#print(pow(2,3))
+'''i=5
+i=i+1
+print(i)
+print('*'*20)'''
+# 6
+#********************
+
+#j=5
+#print(j>2)
+#print(10>j>1)
+#print('abc' > 'acd')
+#falsea
+
+'''a=10
+a += 5
+print(a)
+
+print(True and False)
+print(5>2 or 4<1)'''
+#False
+#True
+#print(not 5>2)
+#False
+'''x=5
+y=3
+print(x and y) # 3'''
+'''x=0
+y=3
+print(x and y) #0 '''
+
+'''x=3
+y=5 
+print(x or y) # 3 '''
+
+'''x=0
+y=5
+print(not x)'''
+
+#print('aaa' if 5<2 else 'bbb') #bbb
+
+'''a=5 #00000101
+b=8 #00001000
+print(a&b)
+print(a |  b)
+print(~a)
+print(a ^ b)
+print(b >> 2) '''
+
+
+'''
+m=[1,3,5,7]
+n=[1,3,5,7]
+d=5
+x=n 
+print(d in m) #True
+print(d not in m) #False
+print(m is n) #False
+print(x is n) #True
+print (x == n) #True '''
+
+
+#names=['tom','alice','jack','mike']
+'''print(names)
+names[0]='lucy'
+print(names)
+print(names[1],names[:3]) # alice ['lucy', 'alice', 'jack']'''
+#names.append('zhangsan')
+#print(names)
+#names.remove('jack')
+#print(names.pop(0))
+#names.append(25)
+#print(names)
+
+'''nums=(3,8,13,25,38)
+print(nums,type(nums)) #(3, 8, 13, 25, 38) <class 'tuple'>
+print(nums[1:3]) #(8,13) '''
+
+nums = [3,8,13,25,38,250]
+a=nums[0]
+b=nums[1]
+c=nums[2]
+d=nums[3]
+e=nums[4]
+f=nums[5]
+print(a, b, c, d, e, f) #3 8 13 25 38 250
+
